386866a3a5ff7dcba5f73f6af078705bbfe6c59f
Thanh21062006 <caotatthanh2006@gmail.com>
2024-12-19 10:09:19 +0700

    Update BT2.cpp

--- BT2.cpp
+++ BT2.cpp
@@ -5,7 +5,7 @@ int main() {
 	int i, arrayInt[5];
 	for(i = 0;i < 5;i++){
 		printf("Moi ban nhap phan tu arrayInt[%d]: ",i);
-		scanf("%p", &arrayInt[i]);
+		scanf("%d", &arrayInt[i]);
 	}
 	printf("\nMang vua nhap la arrayInt[5] = " );
 	for(i = 0;i < 5;i++){
